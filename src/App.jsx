0b7550af84d30f4d3a6ae92bcda6637c9442eea3
Kaveesha Kapuruge <kaveeshakapuruge@gmail.com>
2025-08-22 12:14:12 +0530

    about page added

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import Features from "./pages/Features"; // import your Features page
 // import Dashboard from "./pages/Dashboard"; // if you have it
-// import About from "./pages/About"; // if you have it
+import About from "./pages/about"; // if you have it
 
 function App() {
   const user = { profilePic: null }; 
@@ -12,8 +12,8 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage user={user} />} />
         <Route path="/features" element={<Features />} />
-        {/* <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/about" element={<About />} /> */}
+        {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+        <Route path="/about" element={<About/>} />
       </Routes>
     </Router>
   );
